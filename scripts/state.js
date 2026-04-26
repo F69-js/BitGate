ef8a090294252c240aff1f696121ab107fce0b1a
F69minrcraft <ikoi22323@outlook.jp>
2026-04-26 18:41:18 +0900

    ES Modulesへの完全移行とstate管理の導入により依存関係を解消

---
+++ scripts/state.js
@@ -0,0 +1,11 @@
+export const state = {
+    components: [],
+    wires: [],
+    zoom: 1.0,
+    offset: { x: 0, y: 0 },
+    isSimulating: false,
+    selectedObj: null,
+    draggingObj: null,
+    activeLine: null,
+    mouse: { x: 0, y: 0 }
+};
